ae3814f9a3df31ec9be4deae8f0ae64d8a49b884
Lloyd Evans <contact@lloydevans.co.uk>
2021-07-24 14:48:53 +0200

    chore: update error message

--- src/constants/error-messages.ts
+++ src/constants/error-messages.ts
@@ -5,7 +5,7 @@ export const ERROR_MESSAGES = Object.freeze({
 	INV_NUM: "Invalid number object",
 	INV_V2: "Invalid vec2 object",
 	INV_V2_ARR: "Invalid vec2 array object",
-	INV_V2_TARG: "Invalid vec2 object target",
+	INV_V2_TARG: "Invalid vec2 object",
 	INV_V2_VAL: `Expected value of function to be number | vec2-like array | vec2-like object`,
 	INV_V2_DEST: "Expected destination of function to be number | vec2-like array | vec2-like object",
 	DIVIDE_BY_ZERO: "Detected divide by zero",
