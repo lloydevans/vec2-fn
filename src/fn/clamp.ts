f616c6696e22e00fd121d1eeb8a7cdca4b2e4334
Lloyd Evans <contact@lloydevans.co.uk>
2021-07-24 13:05:09 +0200

    fix: fix `clamp` function
    Fixes bug where providing optional destination object breaks function.

--- src/fn/clamp.ts
+++ src/fn/clamp.ts
@@ -6,31 +6,31 @@ import { min } from "./min";
 /**
  * Clamp a vector by min and max limits.
  *
- * @param vecMin - Minimum vector.
- * @param vecMax - Maximum vector.
+ * @param vMin - Minimum vector.
+ * @param vMax - Maximum vector.
  * @param target - Target object.
  * @returns - A reference to the modified target object.
  */
-export function clamp<T extends Vec2Param, Min extends Vec2Param, Max extends Vec2Param>(target: T, vecMin: Min | number, vecMax: Max | number): T;
+export function clamp<T extends Vec2Param>(target: T, vMin: Vec2Param | number, vMax: Vec2Param | number): T;
 /**
  * Clamp a vector by min and max limits.
  *
- * @param vecMin - Minimum vector.
- * @param vecMax - Maximum vector.
+ * @param vMin - Minimum vector.
+ * @param vMax - Maximum vector.
  * @param target - Target object.
  * @param dest - Destination object to store the values.
  * @returns - A reference to the modified destination object.
  */
-export function clamp<T extends Vec2Param, Min extends Vec2Param, Max extends Vec2Param, D extends Vec2Param>(target: T, vecMin: Min | number, vecMax: Max | number, dest?: D): D;
-export function clamp<T extends Vec2Param, Min extends Vec2Param, Max extends Vec2Param, D extends Vec2Param>(target: T, vecMin: Min | number, vecMax: Max | number, dest?: D): unknown {
-	let result;
-
-	if (dest) {
-		copy(target, dest);
-		result = max(min(dest, vecMax), vecMin);
+export function clamp<D extends Vec2Param>(target: Vec2Param, vMin: Vec2Param | number, vMax: Vec2Param | number, dest: D): D;
+export function clamp<D extends Vec2Param>(target: Vec2Param, vMin: Vec2Param | number, vMax: Vec2Param | number, dest?: D): unknown {
+	if (typeof dest === "undefined") {
+		max(target, target, vMin);
+		min(target, target, vMax);
+		return target;
 	} else {
-		result = max(min(target, vecMax), vecMin);
+		copy(target, dest);
+		max(dest, dest, vMin);
+		min(dest, dest, vMax);
+		return dest;
 	}
-
-	return result;
 }
