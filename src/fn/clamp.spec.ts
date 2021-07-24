e95f880aac514d7da8cbc9ceccff77ff8cf29f73
Lloyd Evans <contact@lloydevans.co.uk>
2021-07-24 13:05:47 +0200

    test: update `clamp` tests

--- src/fn/clamp.spec.ts
+++ src/fn/clamp.spec.ts
@@ -12,7 +12,6 @@ describe(clamp.name, () => {
 		const vec1: Vec2 = { x: 0, y: 0 };
 		const vec2: Vec2 = { x: 0, y: 0 };
 		const dest: Vec2 = { x: 0, y: 0 };
-		expect(() => clamp(null as any, vec2, dest)).toThrowError(ERROR_MESSAGES.INV_V2_DEST);
 		expect(() => clamp(vec1, null as any, dest)).toThrowError(ERROR_MESSAGES.INV_V2_VAL);
 		expect(() => clamp(vec1, vec2, null as any)).toThrowError(ERROR_MESSAGES.INV_V2_VAL);
 	});
@@ -44,13 +43,10 @@ describe(clamp.name, () => {
 	});
 
 	it("should accept mixture of types", () => {
-		const dest1: Vec2 = { x: -1000, y: -10 };
-		const dest2: [number, number] = [-1000, 99];
-		const dest3: Vec2 = { x: 1, y: 0 };
-		const vec1: [number, number] = [0, -1];
-		const vec2: Vec2 = { x: -10, y: 0 };
-		expect(clamp(dest1, vec1, vec2)).toEqual({ x: 0, y: -1 });
-		expect(clamp(dest2, vec1, vec2)).toEqual([0, 0]);
-		expect(clamp(dest3, vec1, vec2)).toEqual({ x: 0, y: 0 });
+		const vecMin: [number, number] = [-100, -1];
+		const vecMax: Vec2 = { x: -10, y: 0 };
+		expect(clamp({ x: -1000, y: -10 }, vecMin, vecMax)).toEqual({ x: -100, y: -1 });
+		expect(clamp([-1000, 99], vecMin, vecMax)).toEqual([-100, 0]);
+		expect(clamp({ x: 1, y: 0 }, vecMin, vecMax)).toEqual({ x: -10, y: 0 });
 	});
 });
